cd5686c301b1dd3e2e234c0a3e47d6b6bef4d693
maciekserwin <maciekserwin@gmail.com>
2023-06-30 13:33:44 +0200

    add guess letter method

--- src/Game.java
+++ src/Game.java
@@ -3,4 +3,20 @@ public class Game {
     private StringBuilder hiddenTitle;
     private StringBuilder wrongGuesses;
     private int wrongGuessCount;
+
+    public void guessLetter(char letter) {
+        letter = Character.toLowerCase(letter);
+        boolean correctGuess = false;
+        for (int i = 0; i < movie.length(); i++) {
+            char c = movie.charAt(i);
+            if (Character.toLowerCase(c) == letter) {
+                hiddenTitle.setCharAt(i, c);
+                correctGuess = true;
+            }
+            if (!correctGuess) {
+                wrongGuesses.append(letter);
+                wrongGuessCount++;
+            }
+        }
+    }
 }
