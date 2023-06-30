cad6cc8c9712301c7d034bd38bc0e0e70854c16b
maciekserwin <maciekserwin@gmail.com>
2023-06-30 11:58:33 +0200

    add ReadAllMoviesFromFile methods including try/catch exception File Not Found

--- src/ListMovie.java
+++ src/ListMovie.java
@@ -1,8 +1,23 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListMovie {
 
     private ArrayList <String> listMovies;
+
+    private void ReadAllMoviesFromFile(String pathname) {
+        listMovies = new ArrayList();
+        File file = new File(pathname);
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                listMovies.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+        }
+    }
 
 }
