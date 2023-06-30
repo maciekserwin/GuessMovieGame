7938ef47953d022d675182081677dd955405971d
maciekserwin <maciekserwin@gmail.com>
2023-06-30 13:43:29 +0200

    add read Guess method

--- src/InputReader.java
+++ src/InputReader.java
@@ -3,4 +3,19 @@ import java.util.Scanner;
 public class InputReader {
 
     private Scanner scanner;
+
+    public char readGuess() {
+        System.out.print("Enter your guess: ");
+        String input = scanner.nextLine().toLowerCase();
+        if (input.length() != 1) {
+            System.out.println("Invalid guess. Please enter a single letter.");
+            return readGuess();
+        }
+        char guess = input.charAt(0);
+        if (!Character.isLetter(guess)) {
+            System.out.println("Invalid guess. Please enter a letter.");
+            return readGuess();
+        }
+        return guess;
+    }
 }
